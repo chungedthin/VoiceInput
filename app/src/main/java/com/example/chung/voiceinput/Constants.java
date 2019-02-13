47371d06ea4cf6a427be30a7ae501f556be0a9de
chungedthin <chungedthin@gmail.com>
2019-02-14 01:34:38 +0800

    uodated on 14/2

---
+++ app/src/main/java/com/example/chung/voiceinput/Constants.java
@@ -0,0 +1,6 @@
+package com.example.chung.voiceinput;
+
+public class Constants {
+    public static final String ROOT_URL = "http://awch.myqnapcloud.com/jj_homepage/an_fyp/OOP/api/user";
+    public static final String URL_REGISTER = ROOT_URL + "/create.php";
+}
--- app/src/main/java/com/example/chung/voiceinput/register.java
+++ app/src/main/java/com/example/chung/voiceinput/register.java
@@ -1,13 +1,39 @@
 package com.example.chung.voiceinput;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
 
@@ -15,6 +41,7 @@ public class register extends AppCompatActivity {
     private EditText password;
     private EditText email;
     private Button registerBtn;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +52,7 @@ public class register extends AppCompatActivity {
         password = (EditText)findViewById(R.id.password);
         email = (EditText)findViewById(R.id.email);
         registerBtn = (Button) findViewById(R.id.registerbtn);
+        progressDialog = new ProgressDialog(this);
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,14 +63,85 @@ public class register extends AppCompatActivity {
     }
 
     private void register (){
-        if(true) {
-            // register success
-            Intent intent = new Intent(register.this, register_success.class);
-            startActivity(intent);
-        }else
-        {
-            // register fail
+        boolean valid = false;
+        String uname = username.getText().toString();
+        String pword = password.getText().toString();
+        String emailaddr = email.getText().toString();
 
+        //validation
+        if(uname.isEmpty() || pword.isEmpty() || emailaddr.isEmpty()) {
+            //register failure
+            Toast.makeText(this,"Sorry,Please enter all the information", Toast.LENGTH_SHORT).show();
+            return;
         }
+        // perform register
+        try {
+            registerUser(uname, pword, emailaddr);
+        } catch (Exception e) {
+            // error
+            e.printStackTrace();
+            return;
+        }
+        // no error occur
+        //...
     }
+
+    private void registerUser(final String email, final String username, final String password) {
+        progressDialog.setMessage( "Registering user...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                            JSONObject parent = jsonObject.getJSONObject("Success");
+                            String value = parent.getJSONObject("message").toString();
+                             if(parent.equals("success")){
+                                 // register success jump to next page
+                                 Intent intent = new Intent(register.this, register_success.class);
+                                 startActivity(intent);
+                             }else {
+                                 // fail
+                             }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("userName", username);
+                params.put("password", password);
+                params.put("email", email);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+        //RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+
+
+    }
+
+   /* @Override
+    public void onClick(View view) {
+        if (view == buttonRegister)
+            registerUser();
+        if(view == textViewLogin)
+            startActivity(new Intent(this, LoginActivity.class));
+    }*/
 }
